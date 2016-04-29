495d2a3150a95fd3720ecdaba0cc46894443605e
sashastip <saschastipanenko@gmail.com>
2016-04-29 09:56:52 +0300

    added auth forms registration

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,14 +9,13 @@ angular.module('rails_api', ['rails', 'ngAnimate', 'ui.bootstrap', 'ui.router', 
       // <-- -------- --> //
 
       // <-- Devise -->//
-      .state('auth_login', { url: '/auth/login', templateUrl: 'auth/login.html', controller: 'AuthCtrl' })
-      .state('auth_signup', { url: '/auth/signup', templateUrl: 'auth/signup.html', controller: 'AuthCtrl' })
-      .state('auth_logout', { url: '/auth/logout', templateUrl: '', controller: 'AuthCtrl' });
+      .state('auth_login', { url: '/auth/login', templateUrl: 'auth/login.html', controller: 'AuthController' });
+      //.state('auth_signup', { url: '/auth/signup', templateUrl: 'auth/signup.html', controller: 'AuthCtrl' });
+      //.state('auth_logout', { url: '/auth/logout', templateUrl: '', controller: 'AuthCtrl' });
       // <-- ----- -->//
 
       //.state('tournaments', { url: '/tournaments', templateUrl: 'tournaments/index.html', controller: 'TournamentCtrl' })
       //.state('tournaments/new', { url: '/tournaments/new', templateUrl: 'tournaments/new.html', controller: 'TournamentCtrl' })
       
-
       $urlRouterProvider.otherwise('/');
 }]);
--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -1,9 +1,7 @@
-'use strict';
-
 angular.module('rails_api')
   .controller('HomeController', ['$scope', '$http', function($scope, $http){
-  	$http.get('/posts.json')
-  	.then(function(res){
-  	  $scope.posts = res.data;
-  	})
+    $http.get('/posts.json')
+      .then(function(res){
+        $scope.posts = res.data;
+      })
   }])
