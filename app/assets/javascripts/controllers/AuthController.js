db42a80d710f579a241547e8eb14ab545d7233c9
sashastip <saschastipanenko@gmail.com>
2016-05-05 14:36:02 +0300

    devise auth for angular

--- app/assets/javascripts/controllers/AuthController.js
+++ app/assets/javascripts/controllers/AuthController.js
@@ -1,40 +1,37 @@
 'use strict';
 
 angular.module('rails_api')
-  .controller('AuthController', ['$scope', '$location', '$http', function($scope, $location, $http){
-  	$scope.user = {email: null, password: null};
+  .controller('AuthController', ['$scope', '$location', '$http', 'Auth', function($scope, $location, $http, Auth){
+  	var config = {
+      headers: {
+        'X-HTTP-Memthod-Override': 'POST'
+      }
+    }
 
      $scope.login = function () {
-      $http({
-        method  : 'POST',
-        url     : '/users',
-        data    : $scope.user
-      })
-        .success(function(data) {
-          if (data.errors) {
-            $scope.errorName = data.errors.name;
-            $scope.errorUserName = data.errors.username;
-            $scope.errorEmail = data.errors.email;
-          } else {
-            $scope.message = data.message;
-          }
+       Auth.login($scope.user, config).then(function(user) {
+         console.log(user);
+       }, function(error) {
+       });
+
+       $scope.$on('devise:login', function(event, currentUser){
+
+       });
+
+       $scope.$on('devise:new-session', function(event, currentUser) {
+            // user logged in by Auth.login({...})
         });
      }
 
      $scope.signup = function () {
-      $http({
-        method  : 'POST',
-        url     : '/users',
-        data    : $scope.user
-      })
-        .success(function(data) {
-          if (data.errors) {
-            $scope.errorName = data.errors.name;
-            $scope.errorUserName = data.errors.username;
-            $scope.errorEmail = data.errors.email;
-          } else {
-            $scope.message = data.message;
-          }
+      Auth.register($scope.user, config).then(function(registeredUser) {
+            console.log(registeredUser); // => {id: 1, ect: '...'}
+        }, function(error) {
+            // Registration failed...
+        });
+
+        $scope.$on('devise:new-registration', function(event, user) {
+            // ...
         });
       }
 
