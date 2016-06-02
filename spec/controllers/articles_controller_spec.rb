cd387be19fc0d08a34f5bebc809b2b8b0a4db295
sashastip <saschastipanenko@gmail.com>
2016-06-02 11:12:53 +0300

    configure devise

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
+  login_user
+
   let(:article) { FactoryGirl.create(:article) }
 
   subject { article }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     email                 { Faker::Internet.email }
     password              "password"
     password_confirmation "password"
+    confirmed_at Date.today
   end
 end
