bef7b248c37f5368f13d8f8947bb9b077df3c378
sashastip <saschastipanenko@gmail.com>
2016-06-27 15:49:43 +0300

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: [:show]
+  def index 
+    @users = User.all 
+  end
+
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def load_user 
+      @user = User.find(params[:id])
+    end
+end
